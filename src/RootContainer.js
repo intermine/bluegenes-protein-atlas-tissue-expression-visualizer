70b83a22bf40188de5d2db361a4bb96f401b1bdf
akshat <akshatbhargavadeepa@gmail.com>
2019-07-02 21:26:21 +0530

    added queryData

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import queryData from './queryData';
 
 class RootContainer extends React.Component {
+	componentDidMount() {
+		const {
+			serviceUrl,
+			entity: { value }
+		} = this.props;
+
+		queryData(value, serviceUrl);
+	}
+
 	render() {
 		return (
 			<div className="rootContainer">
