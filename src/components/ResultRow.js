c56ed16d8ed484521f7c12310f0daf9af8be6dec
akshat <akshatbhargavadeepa@gmail.com>
2019-07-07 16:23:32 +0530

    ResultRow modifications

--- src/components/ResultRow.js
+++ src/components/ResultRow.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import HeatBox from './HeatBox';
 
-function ResultRow({ organ, cellTypes }) {
+const STRENGTHS = {
+	'Not Detected': 0,
+	Low: 1,
+	Medium: 2,
+	High: 3
+};
+
+function ResultRow({ organName, cells = [] }) {
 	return (
 		<tr className="result-row-container">
 			<td className="item">
 				<HeatBox />
 			</td>
-			<td className="item" title={organ}>
-				{organ}
+			<td className="item" title={organName}>
+				{organName}
 			</td>
-			<td className="item">{cellTypes}</td>
+			<td className="item">{cells.length}</td>
 			<td className="item">
-				<HeatBox />
-				<HeatBox />
-				<HeatBox />
+				{cells.map((c, i) => (
+					<HeatBox key={i} strength={STRENGTHS[c.level]} />
+				))}
 			</td>
 		</tr>
 	);
